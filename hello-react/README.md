b8294f26a9b0c96a9260fe276ff0b5adf6b62135
heonjunkim <maplegjswns@naver.com>
2020-11-02 16:41:34 +0900

    오랜만

---
+++ hello-react/README.md
@@ -0,0 +1,7 @@
+## 첫번째 react project
+
+- nodejs 서버를 기반으로 움직이는 Front End Project
+- \*.jsx 확장된 JavaScript언어를 사용하여 화면을 그린다.
+- Component : html tag처럼 열고 닫는 구조로 도구들을 만든다.
+
+*
---
+++ hello-react/src/component/RCC.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class RCC extends Component {
+  render() {
+    return (
+      <div>
+        <p>ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</p>
+      </div>
+    );
+  }
+}
+
+// 외부에서 이 파일의 코드를 import하여
+// 사용할 수 있도록 허가한다.
+export default RCC;
